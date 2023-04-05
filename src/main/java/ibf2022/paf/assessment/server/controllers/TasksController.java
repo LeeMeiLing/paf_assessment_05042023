157019df2f96fe7570468441fb42c787ee3b0502
LeeMeiLing <leem0060@outlook.com>
2023-04-05 10:08:24 +0800

    task 4 completed

--- src/main/java/ibf2022/paf/assessment/server/controllers/TasksController.java
+++ src/main/java/ibf2022/paf/assessment/server/controllers/TasksController.java
@@ -1,8 +1,14 @@
 package ibf2022.paf.assessment.server.controllers;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import ibf2022.paf.assessment.server.models.Task;
 import ibf2022.paf.assessment.server.repositories.UserRepository;
@@ -14,17 +20,39 @@ public class TasksController {
     @Autowired
     UserRepository userRepo;
 
-    // @PostMapping("/task")
-    // public String handleSave(Task task){
-    //     /*
-    //      *  username: fred
-    //         description-0: do%20laundry
-    //         priority-0: 1
-    //         dueDate-0: 2023-04-13
-    //         description-1: buy%20apple
-    //         priority-1: 2
-    //         dueDate-1: 2023-04-07
-    //      */
-    // }
+    // POST /task
+    // Content-Type: application/x-www-form-urlencoded
+    @PostMapping("/task")
+    public String handleSave(@RequestBody MultiValueMap<String,String> payload){
+        /*
+         *  username: fred
+            description-0: do%20laundry
+            priority-0: 1
+            dueDate-0: 2023-04-13
+            description-1: buy%20apple
+            priority-1: 2
+            dueDate-1: 2023-04-07
+         */
+        String username = payload.getFirst("username");
+        Integer totalTask = (payload.size() - 1) / 3; // minus one for username
+        System.out.println(">>> total task = " + totalTask); //debug
+
+        List<Task> tasks = new ArrayList<>();
+        
+        
+        for (int i = 0; i < totalTask; i++){
+            Task task = new Task();
+            task.setUsername(username);
+            task.setDescription(payload.getFirst("description-" + i));
+            task.setPriority(Integer.parseInt(payload.getFirst("priority-" + i)));
+            task.setDueDate(Date.valueOf(payload.getFirst("dueDate-" + i)));
+            tasks.add(task);
+        }
+
+        System.out.println(tasks); // debug
+
+        return null;
+
+    }
     
 }
--- src/main/java/ibf2022/paf/assessment/server/models/Task.java
+++ src/main/java/ibf2022/paf/assessment/server/models/Task.java
@@ -1,8 +1,66 @@
 package ibf2022.paf.assessment.server.models;
 
+import java.sql.Date;
+
 // TODO: Task 4
 
 public class Task {
 
+    /*
+     *  username: fred
+        description-0: do%20laundry
+        priority-0: 1
+        dueDate-0: 2023-04-13
+     */
+    private String username;
+    private String userId;
+    private Integer task_id;
+    private String description;
+    private Integer priority;
+    private Date dueDate;
     
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public Integer getTask_id() {
+        return task_id;
+    }
+    public void setTask_id(Integer task_id) {
+        this.task_id = task_id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Integer getPriority() {
+        return priority;
+    }
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+    public Date getDueDate() {
+        return dueDate;
+    }
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+    @Override
+    public String toString() {
+        return "Task [username=" + username + ", userId=" + userId + ", task_id=" + task_id + ", description="
+                + description + ", priority=" + priority + ", dueDate=" + dueDate + "]";
+    }
+
+    
+
 }
